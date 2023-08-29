8d63f8f2d23bbc6d1c232011f8b5068ffc754db9
EdrinM <edrinmolla@gmail.com>
2023-08-29 18:13:27 +0200

    added part of waypoints. Added red marker for later

--- app/src/main/java/com/example/buslist/OpenStreetMapActivity.kt
+++ app/src/main/java/com/example/buslist/OpenStreetMapActivity.kt
@@ -3,6 +3,7 @@ package com.example.buslist
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -32,13 +33,18 @@ class OpenStreetMapActivity : AppCompatActivity() {
         mapView.setMultiTouchControls(true)
 
         val stationName = intent.getStringExtra("stationName")
+        Log.d("Debugging", "Selected Station: $stationName") // Debug log
+
         val routeJsonFileName = stationName?.let { getJsonFileNameForStation(it) }
         val routeJsonContent: String? = routeJsonFileName?.let { loadJSONFromAsset(it) }
 
-        val stationsJsonFileName = "selitastations.json"
-        val stationsJsonContent: String? = loadJSONFromAsset(stationsJsonFileName)
+        val stationsJsonFileName = getWaypointJsonFileNameForStation(stationName)
+        Log.d("Debugging", "Waypoint JSON File: $stationsJsonFileName") // Debug log
+        val stationsJsonContent: String? = stationsJsonFileName?.let { loadJSONFromAsset(it) }
 
         if (routeJsonContent != null && stationsJsonContent != null) {
+            mapView.overlays.clear() // Clear existing overlays before adding new ones
+
             val (parsedLines, parsedDescriptions) = parseRouteJSON(routeJsonContent)
             descriptions = parsedDescriptions
 
@@ -82,25 +88,40 @@ class OpenStreetMapActivity : AppCompatActivity() {
             else -> "default.json" // Replace with your default JSON file
         }
     }
+    private fun getWaypointJsonFileNameForStation(stationName: String?): String? {
+        return when (stationName) {
+            "Kombinat - Kinostudio" -> "kombinat_kinostudio_waypoints.json"
+            "Tirana e Re" -> "tirana_re_waypoints.json"
+            "Unaza" -> "unaza_waypoints.json"
+            "Selite" -> "sharre_uzinadinamo_waypointstemp.json"
+            "Porcelan-Qender" -> "porcelan_qender_waypoints.json"
+            "Sauk - Qender" -> "sauk_qender_waypoints.json"
+            "Uzina Traktori - Qender" -> "uzinatraktori_qender_waypoints.json"
+            "Instituti Bujqesor - Qender" -> "institut_qender_waypoints.json"
+            "St Trenit - Qender - TEG" -> "treni_qender_teg_waypoints.json"
+            "Tufine - Kombinat" -> "tufine_kombinat_waypoints.json"
+            "Sharre - Uzina Dinamo" -> "sharre_uzinadinamo_waypointstemp.json"
+            else -> null
+        }
+    }
 
-    private fun parseStationsJSON(json: String): List<Pair<Double, Double>> {
-        val parsedWaypoints = mutableListOf<Pair<Double, Double>>()
+    private fun parseStationsJSON(json: String): List<GeoPoint> {
+        val parsedWaypoints = mutableListOf<GeoPoint>()
 
         try {
-            val jsonObject = JSONObject(json)
-            val featuresArray = jsonObject.getJSONArray("features")
+            val jsonArray = JSONArray(json)
 
-            for (i in 0 until featuresArray.length()) {
-                val feature = featuresArray.getJSONObject(i)
-                val geometry = feature.getJSONObject("geometry")
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray.getJSONObject(i)
+                val geometry = jsonObject.getJSONObject("geometry")
                 val type = geometry.getString("type")
 
-                if (type == "Feature") {
+                if (type == "Point") {
                     val coordinates = geometry.getJSONArray("coordinates")
                     val lon = coordinates.getDouble(0)
                     val lat = coordinates.getDouble(1)
 
-                    parsedWaypoints.add(Pair(lat, lon))
+                    parsedWaypoints.add(GeoPoint(lat, lon))
                 }
             }
         } catch (e: Exception) {
@@ -109,6 +130,7 @@ class OpenStreetMapActivity : AppCompatActivity() {
 
         return parsedWaypoints
     }
+
 
 
     private fun parseRouteJSON(json: String): Pair<List<List<Pair<Double, Double>>>, List<String>> {
@@ -146,18 +168,19 @@ class OpenStreetMapActivity : AppCompatActivity() {
         return Pair(parsedLines, parsedDescriptions)
     }
 
-    private fun addMarkersToMap(mapView: MapView, waypoints: List<Pair<Double, Double>>, descriptions: List<String>) {
+    private fun addMarkersToMap(mapView: MapView, waypoints: List<GeoPoint>, descriptions: List<String>) {
         for (i in waypoints.indices) {
-            val (lat, lon) = waypoints[i]
+            val waypoint = waypoints[i]
             val description = if (i < descriptions.size) descriptions[i] else ""
 
             val marker = Marker(mapView)
-            marker.position = GeoPoint(lat, lon)
+            marker.position = waypoint
             marker.title = description
             mapView.overlays.add(marker)
         }
         Log.d("Debugging", "Markers added: ${waypoints.size}")
     }
+
 
     private fun loadJSONFromAsset(fileName: String): String? {
         var jsonContent: String? = null
